aa79fd59937ee299eb5c103552856b1a6531dfc5
Hector Lopez <hectorhlopez@192.168.1.18>
2025-10-07 15:11:04 -0600

    update: custom logo and CSP fixes

---
+++ components/StudioLogo.tsx
@@ -0,0 +1,12 @@
+// components/StudioLogo.tsx
+import React from 'react';
+
+export default function StudioLogo() {
+  return (
+    <img
+      src="/favicon.svg"   // or /favicon.png
+      alt="BeNeXT HQ"
+      style={{ height: 24 }}
+    />
+  );
+}
