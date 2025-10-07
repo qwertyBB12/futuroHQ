e816abba45121bcf079590f5fef2628dfab88efe
Hector Lopez <hectorhlopez@192.168.1.18>
2025-10-07 15:22:29 -0600

    feat(studio): use PNG for header logo and bust favicon cache

--- components/StudioLogo.tsx
+++ components/StudioLogo.tsx
@@ -2,11 +2,12 @@
 import React from 'react';
 
 export default function StudioLogo() {
+  // Prefer a crisp PNG; 24px tall looks good in the Studio header
   return (
     <img
-      src="/favicon.svg"   // or /favicon.png
+      src="/favicon-32x32.png"   // or "/apple-touch-icon.png" or "/favicon.ico"
       alt="BeNeXT HQ"
-      style={{ height: 24 }}
+      style={{ height: 24, width: 'auto' }}
     />
   );
 }
